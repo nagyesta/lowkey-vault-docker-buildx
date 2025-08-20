328b20dc4539364af51bfc75a9de279bbff82cbb
Esta Nagy <nagyesta@gmail.com>
2025-08-20 16:59:22 +0200

    chore: Update to JDK21 - Migrate Docker base image to JDK 21 - Migrate lowkey-vault version to 4.0.0 - Update build workflows to use JDK 21
    Updates #1602
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,9 +76,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
